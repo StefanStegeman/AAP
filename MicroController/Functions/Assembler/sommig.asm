edec554a0eaa5e9d7fa0c336ad41246f5f2f1e0a
StefanStegeman <s.stegeman1806@gmail.com>
2022-06-30 21:19:46 -0700

    Finished.

--- MicroController/Functions/Assembler/sommig.asm
+++ MicroController/Functions/Assembler/sommig.asm
@@ -1,18 +1,18 @@
 .cpu cortex-m0
 .text
 .align 2
-.global factorial
+.global sommig
 
-factorial:
+sommig:
 	PUSH 	{ R4, LR }
-	MOVS	R1, #1
+	MOVS	R1, #0
 LOOP:
-	MOVS	R2, #0
-	MOV 	R3, #1
+	MOVS	R2, #1
+	ASR 	R3, R0, #31
+	LSR 	R4, R2, #31
 	CMP 	R0, R2
-	BGT 	.L2
-	MOVS	R3, #0
-	MUL 	R1, R0
+	ADC 	R3, R3, R4
+	ADD 	R1, R0
 	MOVS	R4, #1
 	SUB 	R0, R4
 	CMP 	R3, #1
--- MicroController/main.cpp
+++ MicroController/main.cpp
@@ -9,17 +9,24 @@ extern "C" bool less(int a, int b);
 extern "C" bool lessEquals(int a, int b);
 extern "C" bool notEquals(int a, int b);
 extern "C" bool orTest(int a, int b);
+extern "C" bool odd(int n);
+extern "C" bool even(int n);
+extern "C" int sommig(int n);
 
 int main (void)
 {
     hwlib::wait_ms(2000);
-    hwlib::cout << "Testing and:\t\t" << andTest(10, 7) << " \tNeeds to be: 1" << "\n";
-    hwlib::cout << "Testing equals:\t\t" << equals(5, 5) << " \tNeeds to be: 1" << "\n";
-    hwlib::cout << "Testing greater:\t" << greater(15, 2) << " \tNeeds to be: 1" << "\n";
-    hwlib::cout << "Testing greaterEquals:\t" << greaterEquals(2, 3) << " \tNeeds to be: 0" << "\n";
-    hwlib::cout << "Testing if:\t\t" << ifTest(7) << " \tNeeds to be: 1" << "\n";
-    hwlib::cout << "Testing less:\t\t" << less(7, 1) << " \tNeeds to be: 0"<< "\n";
-    hwlib::cout << "Testing lessEquals:\t" << lessEquals(4, 4) << " \tNeeds to be: 1"<< "\n";
-    hwlib::cout << "Testing notEquals:\t" << notEquals(10, 15) <<" \tNeeds to be: 1"<< "\n";
-    hwlib::cout << "Testing or:\t\t" << orTest(2, 2)<<" \tNeeds to be: 1"<< "\n";
+    hwlib::cout << "Testing and:\t\t" << andTest(10, 7) << " \tNeeds to be: " << (10 == 10 && 7 == 7) << "\n";
+    hwlib::cout << "Testing equals:\t\t" << equals(5, 5) << " \tNeeds to be: " << (5 == 5) << "\n";
+    hwlib::cout << "Testing greater:\t" << greater(15, 2) << " \tNeeds to be: " << (15 > 2) << "\n";
+    hwlib::cout << "Testing greaterEquals:\t" << greaterEquals(2, 3) << " \tNeeds to be: " << (2 >= 3) << "\n";
+    hwlib::cout << "Testing if:\t\t" << ifTest(7) << " \tNeeds to be: " << (7 == 7) << "\n";
+    hwlib::cout << "Testing less:\t\t" << less(7, 1) << " \tNeeds to be: "<< (7 < 1) << "\n";
+    hwlib::cout << "Testing lessEquals:\t" << lessEquals(4, 4) << " \tNeeds to be: "<< (4 <= 4) << "\n";
+    hwlib::cout << "Testing notEquals:\t" << notEquals(10, 15) <<" \tNeeds to be: "<< (10 != 15) << "\n";
+    hwlib::cout << "Testing or:\t\t" << orTest(2, 3)<<" \tNeeds to be: "<< (2 != 2 || 3 == 3) << "\n";
+    
+    hwlib::cout << "Testing odd:\t\t" << odd(3)<<" \tNeeds to be: "<< (3 % 2 != 0) <<"\n";
+    hwlib::cout << "Testing even:\t\t" << even(12)<<" \tNeeds to be: "<< (12 % 2 == 0) <<"\n";
+    hwlib::cout << "Testing sommig:\t\t" << sommig(12)<<" \tNeeds to be: 78"<<"\n";
 }
